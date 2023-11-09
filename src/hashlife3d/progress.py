f3f59e8bb0c8b4b106fb6cab4e0dbc4fb492625b
Jo Liss <joliss42@gmail.com>
2023-11-09 00:52:58 +0000

    More detailed progress

--- src/hashlife3d/progress.py
+++ src/hashlife3d/progress.py
@@ -1,11 +1,17 @@
 from typing import Iterator
 
+import psutil
+from pympler import asizeof
+
+from .canonical import _intern_pool
+
 
 _do_show_progress = True
 def set_do_show_progress(value):
     global _do_show_progress
     _do_show_progress = value
 
+_process = psutil.Process()
 
 def unwrap_with_progress_bar(iterator):
     assert isinstance(iterator, Iterator)
@@ -15,7 +21,13 @@ def unwrap_with_progress_bar(iterator):
             progress = next(iterator)
             n += 1
             if n % 1000 == 0 and _do_show_progress:
-                print(f'\rProgress: {progress}', end="", flush=True)
+                progress_stats = ' '.join(f'{n:2n}' for n in progress)
+                memory = _process.memory_info().rss
+                memory_stats = f', {len(_intern_pool):12n} objects, {int(memory) // 1024 // 1024:7n} MB allocated'
+                # memory_stats += f', {int(asizeof.asizeof(_intern_pool) / len(_intern_pool))} accurate bytes'
+                # memory_stats += f', {int(asizeof.asizeof(list(_intern_pool.keys())) / len(_intern_pool))} key bytes'
+                # memory_stats += f', {int(asizeof.asizeof(list(_intern_pool.values())) / len(_intern_pool))} value bytes'
+                print(f'\rProgress: {progress_stats}{memory_stats}', end="", flush=True)
     except StopIteration as e:
         print(flush=True)
         return e.value
